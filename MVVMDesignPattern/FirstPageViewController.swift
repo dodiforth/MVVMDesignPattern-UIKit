7e7e567582d732248578d46d24772b50cb9c24bf
dodinoyo <dowoniscool@yahoo.com>
2023-08-05 16:27:41 +0200

    Initial Commit

---
+++ MVVMDesignPattern/FirstPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstPageViewController.swift
+//  MVVMDesignPattern
+//
+//  Created by Dowon Kim on 05/08/2023.
+//
+
+import UIKit
+
+class FirstPageViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
